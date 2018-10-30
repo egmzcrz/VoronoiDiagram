54adff83ecc17a693dd0cdcad4ba70821aeb7afa
Enrique Gómez Cruz <enriquegomezcruz@gmail.com>
2018-10-30 01:07:23 -0600

    added a test example.

--- src/voronoi.jl
+++ src/voronoi.jl
@@ -8,11 +8,14 @@ struct Point
     y::Float64
 end
 
-mutable struct HalfEdge
-    p::Point
-    q::Point
-    direction::Point
-    HalfEdge(p::Point,direction::Point) = new(p,p,direction)
+# A half-edge grows parallel to the bisector
+# between two sites. If half-edge grows in the direction
+# v, then its opposite grows in th direction -v
+mutable struct Halfedge
+    start::Point
+    finish::Point
+    opposite::Halfedge
+    Halfedge(p) = new(p,p)
 end
 
 # The beachline is represented by a binary search tree, it has
@@ -27,28 +30,25 @@ mutable struct BST
 end
 
 mutable struct Breakpoint <: VoronoiNode
-    siteL::Point
-    siteR::Point
-    halfedge::HalfEdge
-
+    lsite::Point
+    rsite::Point
+    halfedge::Halfedge
     parent::Union{VoronoiNode,Nothing}
-    childL::Union{VoronoiNode,Nothing}
-    childR::Union{VoronoiNode,Nothing}
-
-    function Breakpoint(p::Point, q::Point, halfedge::HalfEdge)
-        return new(p,q,halfedge,nothing,nothing,nothing)
-    end
+    lchild::Union{VoronoiNode,Nothing}
+    rchild::Union{VoronoiNode,Nothing}
+    Breakpoint(lsite, rsite, halfedge) = new(lsite, rsite, halfedge,
+                                             nothing,nothing,nothing)
 end
 
 mutable struct Arc <: VoronoiNode
     site::Point
     circleEvent::Union{VoronoiEvent,Nothing}
     parent::Union{VoronoiNode,Nothing}
-    Arc(p::Point) = new(p,nothing,nothing)
+    Arc(site) = new(site,nothing,nothing)
 end
 
-# A priority queue handles events. Events have reference to points.
-# They are ordered based on their point's position.
+# A priority queue handles the events. Events have references to points.
+# They are ordered based on their point's location.
 # At site events arcs are created.
 # At circle events arcs vanish.
 struct SiteEvent <: VoronoiEvent
@@ -62,6 +62,8 @@ mutable struct CircleEvent <: VoronoiEvent
     CircleEvent(p::Point, arc::VoronoiNode) = new(p,arc,false)
 end
 
+# Overload ">" to allow comparing Voronoi events.
+# This is needed for the priority queue to work with our types.
 import Base.>
 function >(e1::VoronoiEvent, e2::VoronoiEvent)
     p = e1.p
@@ -70,32 +72,32 @@ function >(e1::VoronoiEvent, e2::VoronoiEvent)
     return p.y > q.y
 end
 
-# Parabola with focal point, f, and directrix ly evaluated at x
+# Parabola with focal point f, and directrix ly evaluated at x
 function parabola(x::Float64, ly::Float64, f::Point)
     a = 2*(f.y - ly)
     b = f.x*f.x + f.y*f.y - ly*ly
     return (x*x - 2*x*f.x + b)/a
 end
 
-# ly is sweepline's y-coordinate
-# p and q represent the foci of the parabolas they define.
-function calculateBreakpoint(p::Point, q::Point, ly::Float64)
-    if p.y == q.y; return (p.x + q.x) / 2; end
+# fa and fb are focal points, ly is the directrix.
+function getBreakpoint(fa::Point, fb::Point, ly::Float64)
+    if fa.y == fb.y; return (fa.x + fb.x) / 2; end
     # intersection of two parabolas
-    a = p.y - ly
-    b = q.y - ly
-    c = p.x - q.x
-    d = p.y - q.y
-    s1 = a*q.x - b*p.x
+    a = fa.y - ly
+    b = fb.y - ly
+    c = fa.x - fb.x
+    d = fa.y - fb.y
+    s1 = a*fb.x - b*fa.x
     s2 = sqrt((c*c + d*d)*a*b)
     
-    # There are two solutions: x1, x2, we want the point whose
-    # x-coordinate is in between both sites.
-    x1 = (s1 - s2) / d
-    x2 = (s1 + s2) / d
-    return p.y < q.y ? max(x1,x2) : min(x1,x2)
+    # There are two solutions: x1 = (s1-s2)/d and x2 = (s1+s2)/d.
+    # Also, x1 < x2 iff d > 0.
+    # So x1 will throw the point to the left or to the right of the
+    # lowest parabola, which is exactly what we want.
+    return (s1 - s2) / d
 end
 
+# a, b and c define a circumcircle.
 function getCircleLowestPoint(a::Point, b::Point, c::Point)
     ux = b.x - a.x; uy = b.y - a.y
     vx = c.x - b.x; vy = c.y - b.y
@@ -115,6 +117,9 @@ function getCircleLowestPoint(a::Point, b::Point, c::Point)
     return Point(x,y-r)
 end
 
+# ccw checks if the orientation of the ordered triplet (a,b,c)
+# is counter-clockwise.
+# Collinearity is when v×w = 0, so numerical errors should be considered.
 function ccw(a::Point, b::Point, c::Point)
     abx = b.x - a.x
     aby = b.y - a.y
@@ -122,93 +127,38 @@ function ccw(a::Point, b::Point, c::Point)
     acy = c.y - a.y
 
     crossProduct = abx * acy - aby * acx
-    # ccw will take into account crossProduct >= 0,
-    # that is, collinearity is accounted for.
     return crossProduct > -1e-15
 end
 
+# Search for the arc directly on top of current site.
 function getArcOnTop(beachline::BST, site::Point)
     node = beachline.root
     while typeof(node) === Breakpoint
-        x = calculateBreakpoint(node.siteL, node.siteR, site.y)
-        node = site.x < x ? node.childL : node.childR
+        x = getBreakpoint(node.lsite, node.rsite, site.y)
+        node = site.x < x ? node.lchild : node.rchild
     end
     return node
 end
 
-function getLeftBreakpoint(node::Arc)
-    parent = node.parent
-    if parent === nothing; return nothing; end
-    while parent.childL === node
-        if parent.parent === nothing; return nothing; end
-        node = parent
-        parent = parent.parent
-    end
-    return parent
-end
-function getRightBreakpoint(node::Arc)
-    parent = node.parent
-    if parent === nothing; return nothing; end
-    while parent.childR === node
-        if parent.parent === nothing; return nothing; end
-        node = parent
-        parent = parent.parent
-    end
-    return parent
-end
-function getLeftArc(node::Breakpoint)
-    if node === nothing; return nothing; end
-    child = node.childL
-    while typeof(child) === Breakpoint
-        child = child.childR
-    end
-    return child
-end
-function getRightArc(node::Breakpoint)
-    if node === nothing; return nothing; end
-    child = node.childR
-    while typeof(child) === Breakpoint
-        child = child.childL
-    end
-    return child
-end
-function setRightChild!(node::Breakpoint, child::VoronoiNode)
-    node.childR = child
-    child.parent = node
-end
-function setLeftChild!(node::Breakpoint, child::VoronoiNode)
-    node.childL = child
-    child.parent = node
-end
-
-function checkCircleEvent(arc::Arc, events::Vector{VoronoiEvent})
-    # Get arc's left and right neighbouring arcs.
-    lbp = getLeftBreakpoint(arc)
-    rbp = getRightBreakpoint(arc)
-    if lbp === nothing || rbp === nothing; return; end
-    la = getLeftArc(lbp)
-    ra = getRightArc(rbp)
-    if la === nothing || ra === nothing || la.site === ra.site; return; end
-
-    # Check if edges <la,arc> and <arc,ra> will converge.
+function checkCircleEvent(la::Arc, ma::Arc, ra::Arc,
+                          events::Vector{VoronoiEvent})
+    # Check if edges <la,ma> and <ma,ra> converge.
     a = la.site
-    b = arc.site
+    b = ma.site
     c = ra.site
     if ccw(a,b,c); return; end
 
-    # Edges will converge at the circumcenter of the circle defined
-    # by the sites of la, arc, and ra. The circle event will then take
-    # place when the sweep line reaches the bottom of that circle.
-    #
-    # Create circle event pointing to arc.
+    # Edges will converge at the circumcenter defined by a,b,c.
+    # The circle event will then take place when the sweep line
+    # reaches the bottom of that circle.
     p = getCircleLowestPoint(a,b,c)
-    circleEvent = CircleEvent(p,arc)
+    # Create circle event pointing to arc.
+    circleEvent = CircleEvent(p,ma)
     # Point arc to circle event
-    arc.circleEvent = circleEvent
+    ma.circleEvent = circleEvent
 
     enqueue!(events, circleEvent)
 end
-
 
 function markAsFalseAlarm!(event::CircleEvent)
     event.isFalseAlarm = true
@@ -216,8 +166,10 @@ function markAsFalseAlarm!(event::CircleEvent)
     event.arc = nothing
 end
 
-function handleSiteEvent(site::Point, beachline::BST,
-                         events::Vector{VoronoiEvent}, edges::Vector{HalfEdge})
+function handleSiteEvent!(beachline::BST,
+                          events::Vector{VoronoiEvent},
+                          event::SiteEvent)
+    site = event.p
     if beachline.root === nothing
         beachline.root = Arc(site)
         return
@@ -231,31 +183,27 @@ function handleSiteEvent(site::Point, beachline::BST,
         markAsFalseAlarm!(arc.circleEvent)
     end
 
-    # Start new edges at breakpoint (intersection between
+    # Start new half-edges at breakpoint (intersection between
     # current site's parabola and the arc directly on top)
     # Edges will grow in the directions parallel to the
-    # bisector between site and arc's focus.
+    # bisector between site and arc's site.
     x = site.x
     y = parabola(x, site.y, arc.site)
     start = Point(x,y)
-    # A vector pointing from current site to its arc on top.
-    rx = arc.site.x - site.x
-    ry = arc.site.y - site.y
-    # (-ry,rx) points to the left parallel to the bisector.
-    # (ry,-rx) points to the right parallel to the bisector.
-    el = HalfEdge(start, Point(-ry,rx))
-    er = HalfEdge(start, Point(ry,-rx))
-    push!(edges,el,er)
+    lhe = Halfedge(start)
+    rhe = Halfedge(start)
+    lhe.opposite = rhe
+    rhe.opposite = lhe
 
 
-    # Replace arc-on-top with new subtree:
-    #   lbp
+    # Replace arc with following subtree:
+    #   lbp ~~~~> lhe
     #  /   \
-    # la   rbp
+    # la   rbp ~~~~> rhe
     #     /   \
     #    ma   ra
-    lbp = Breakpoint(arc.site, site, el)
-    rbp = Breakpoint(site, arc.site, er)
+    lbp = Breakpoint(arc.site, site, lhe)
+    rbp = Breakpoint(site, arc.site, rhe)
     la = Arc(arc.site)
     ma = Arc(site)
     ra = Arc(arc.site)
@@ -264,7 +212,7 @@ function handleSiteEvent(site::Point, beachline::BST,
     setLeftChild!(rbp, ma)
     setRightChild!(rbp, ra)
     if arc.parent !== nothing
-        if arc.parent.childL === arc
+        if arc.parent.lchild === arc
             setLeftChild!(arc.parent, lbp)
         else
             setRightChild!(arc.parent, lbp)
@@ -273,16 +221,39 @@ function handleSiteEvent(site::Point, beachline::BST,
         beachline.root = lbp
     end
 
-    # Rebalancing operations?
+    # TODO: Rebalancing operations.
 
-    # Left or right arcs could eventually disappear
-    # check for their circle events.
-    checkCircleEvent(la,events)
-    checkCircleEvent(ra,events)
+    # Left or right arcs could eventually disappear,
+    # check for potential circle events.
+    llbp = getLeftBreakpoint(la)
+    rrbp = getRightBreakpoint(ra)
+    if llbp !== nothing
+        lla = getLeftArc(llbp)
+        if lla.site !== ma.site; checkCircleEvent(lla,la,ma,events); end
+    end
+    if rrbp !== nothing
+        rra = getRightArc(rrbp)
+        if ma.site !== rra.site; checkCircleEvent(ma,ra,rra,events); end
+    end
 end 
 
-function handleCircleEvent(event::CircleEvent, events::Vector{VoronoiEvent},
-                           edges::Vector{HalfEdge})
+# Finishes edges that already start at a vertex or starts them at one.
+# An edge that starts at a voronoi vertex shouldn't have an opposite
+# edge since a vertex is like a source of edges.
+function connectEdge2Vertex!(edge::Halfedge, p::Point, edges::Vector{Halfedge})
+    if edge.opposite !== edge
+        edge.start = p
+        edge.opposite = edge
+    else
+        edge.finish = p
+        push!(edges, edge)
+    end
+end
+
+function handleCircleEvent!(edges::Vector{Halfedge},
+                           events::Vector{VoronoiEvent},
+                           event::CircleEvent)
+    if event.isFalseAlarm; return; end
     # The arc that will disappear.
     arc = event.arc
 
@@ -299,60 +270,82 @@ function handleCircleEvent(event::CircleEvent, events::Vector{VoronoiEvent},
         markAsFalseAlarm!(ra.circleEvent)
     end
 
-    # lbp's and rbp's edges will meet at circumcenter.
-    # Finish those edges.
+    # Get the point of intersection.
     x = event.p.x
-    y = parabola(x, event.p.y, la.site)
-    finish = Point(x,y)
-    lbp.halfedge.q = finish
-    rbp.halfedge.q = finish
+    ly = event.p.y
+    y = parabola(x, ly, la.site) # la's, ra's or arc's site will do.
+    circumcenter = Point(x,y)
 
-    # lbp and rbp are the only nodes that reference the arc that will disappear.
-    # One of them is the direct parent of the disappearing arc and it
-    # has to be removed. The other needs to be updated to reference the new
-    # growing edge between la and ra.
-    innerMost = arc.parent === lbp ? rbp : lbp
-    innerMost.siteL = la.site
-    innerMost.siteR = ra.site
-    rx = la.site.x - ra.site.x
-    ry = la.site.y - ra.site.y
-    edge = HalfEdge(finish, Point(-ry,rx))
-    push!(edges, edge)
-    innerMost.halfedge = edge
+    # lbp and rbp's breakpoints will be updated, so their edges will stop
+    # existing. But the opposite edges will keep on growing.
+    le = lbp.halfedge.opposite
+    re = rbp.halfedge.opposite
+    connectEdge2Vertex!(le, circumcenter, edges)
+    connectEdge2Vertex!(re, circumcenter, edges)
 
-    # Save the node that is not the disappearing arc and connect it
-    # to arc's grandparent.
-    gparent = arc.parent.parent
-    parent = arc.parent
-    node = parent.childL === arc ? parent.childR : parent.childL
-    if gparent.childL === parent
-        setLeftChild!(gparent,node)
-    else
-        setRightChild!(gparent,node)
+    # lbp and rbp will be updated. One of them is the direct parent of arc.
+    # The one that is the direct parent can be removed. The other one
+    # needs to be updated to manage the breakpoint <la,ra>.
+    #     lbp: <la,arc>            lbp: <la,ra>
+    #    /    \                   /   \
+    #   x     gp                 x    gp
+    #  / \    / \      ==>      / \   / \
+    # x  la  rbp x             x  la ra  x
+    #       /  \
+    #     arc   ra
+    innerMost = arc.parent === rbp ? lbp : rbp
+    innerMost.lsite = la.site
+    innerMost.rsite = ra.site
+    gp = arc.parent.parent # grandparent
+    dp = arc.parent # direct parent
+    node = dp.lchild === arc ? dp.rchild : dp.lchild
+    gp.lchild === dp ? setLeftChild!(gp,node) : setRightChild!(gp,node)
+    newEdge = Halfedge(circumcenter)
+    newEdge.opposite = newEdge # newEdge starts at a voronoi vertex.
+    innerMost.halfedge = newEdge
+
+    # Check for potential circle events.
+    llbp = getLeftBreakpoint(la)
+    rrbp = getRightBreakpoint(ra)
+    if llbp !== nothing
+        lla = getLeftArc(llbp)
+        checkCircleEvent(lla,la,ra,events);
     end
-
-    checkCircleEvent(la,events)
-    checkCircleEvent(ra,events)
+    if rrbp !== nothing
+        rra = getRightArc(rrbp)
+        checkCircleEvent(la,ra,rra,events);
+    end
 end
 
-function finishEdges!(beachline::BST)
+function clipEdges!(edges::Vector{Halfedge}, bbox::Vector{NTuple{2,Point}})
+end
+
+function finishEdges!(beachline::BST, edges::Vector{Halfedge})
     node = beachline.root
     if typeof(node) === Arc; return; end
     breakpts::Vector{Breakpoint} = [node]
     while length(breakpts) > 0
         node = pop!(breakpts)
-        childL = node.childL
-        childR = node.childR
-        if typeof(childL) === Breakpoint
-            push!(breakpts, childL)
+        lsite = node.lsite
+        rsite = node.rsite
+        # Direction vector.
+        rx = rsite.x - lsite.x
+        ry = rsite.y - lsite.y
+        # Grow half-edge parallel to direction vector.
+        edge = node.halfedge
+        x = edge.start.x + 5*ry
+        y = edge.start.y - 5*rx
+        edge.finish = Point(x,y)
+        push!(edges, edge)
+
+        lchild = node.lchild
+        rchild = node.rchild
+        if typeof(lchild) === Breakpoint
+            push!(breakpts, lchild)
         end
-        if typeof(childR) === Breakpoint
-            push!(breakpts, childR)
+        if typeof(rchild) === Breakpoint
+            push!(breakpts, rchild)
         end
-        e = node.halfedge
-        x = e.p.x + e.direction.x 
-        y = e.p.y + e.direction.y 
-        e.q = Point(x,y)
     end
 end
 
@@ -363,21 +356,72 @@ function getVoronoiDiagram(sites::Vector{Point})
         enqueue!(events, SiteEvent(site))
     end
 
-    # Initialize empty beachline
+    # Initialize empty beachline.
     beachline::BST = BST()
 
     # Initialize empty edge list
-    edges::Vector{HalfEdge} = []
+    edges::Vector{Halfedge} = []
 
     while length(events) > 0
-        event = dequeue!(events)
+        event::VoronoiEvent = dequeue!(events)
         if typeof(event) === SiteEvent
-            handleSiteEvent(event.p, beachline, events, edges)
-        elseif !event.isFalseAlarm
-            handleCircleEvent(event, events, edges)
+            handleSiteEvent!(beachline, events, event)
+        else
+            handleCircleEvent!(edges, events, event)
         end
     end
 
-    finishEdges!(beachline)
+    # Make edges fit inside bounding box.
+    #clipEdges!(edges,bbox)
+    finishEdges!(beachline, edges)
     return edges
 end
+
+###################################################################
+# BST utility functions
+###################################################################
+function getLeftBreakpoint(node::Arc)
+    parent = node.parent
+    if parent === nothing; return nothing; end
+    while parent.lchild === node
+        if parent.parent === nothing; return nothing; end
+        node = parent
+        parent = parent.parent
+    end
+    return parent
+end
+function getRightBreakpoint(node::Arc)
+    parent = node.parent
+    if parent === nothing; return nothing; end
+    while parent.rchild === node
+        if parent.parent === nothing; return nothing; end
+        node = parent
+        parent = parent.parent
+    end
+    return parent
+end
+function getLeftArc(node::Breakpoint)
+    if node === nothing; return nothing; end
+    child = node.lchild
+    while typeof(child) === Breakpoint
+        child = child.rchild
+    end
+    return child
+end
+function getRightArc(node::Breakpoint)
+    if node === nothing; return nothing; end
+    child = node.rchild
+    while typeof(child) === Breakpoint
+        child = child.lchild
+    end
+    return child
+end
+function setRightChild!(node::Breakpoint, child::VoronoiNode)
+    node.rchild = child
+    child.parent = node
+end
+function setLeftChild!(node::Breakpoint, child::VoronoiNode)
+    node.lchild = child
+    child.parent = node
+end
+###################################################################
